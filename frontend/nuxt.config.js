cb4128f7fc532ab6317595a410814d8f8e7027dc
AndreyCheschevoy <andreyosipenko@ukr.net>
2019-12-28 20:56:17 +0200

    init

---
+++ frontend/nuxt.config.js
@@ -0,0 +1,64 @@
+
+export default {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css' }
+    ],
+    script: [
+      {
+        type: 'text/javascript', href: 'https://code.jquery.com/jquery-3.4.1.slim.min.js',
+      },
+      {
+        type: 'text/javascript', href: 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js'
+      },
+      {
+        type: 'text/javascript', href: 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js'
+      },
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+  ],
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    }
+  }
+}
---
+++ frontend/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    home
+  </div>
+</template>
+
+<script>
+import Nav from '~/components/Navbar.vue'
+
+export default {
+  components: {
+    Nav
+  }
+}
+</script>
